656283111b5876eb157b7ac1a5e7dbd00edbd6ad
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-07-12 16:06:24 +0900

    object mapper 실습

---
+++ java/SpringBoot/FastCampusJavaSpring/object-mapper/src/test/java/com/example/objectmapper/ObjectMapperApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.objectmapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectMapperApplicationTests {
+
+    @Test
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("hello");
+
+        var objectMapper = new ObjectMapper();
+
+        var user = new User("steve", 10, "010-1111-1111");
+
+        // object -> text
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        // text -> object
+        var objectUser = objectMapper.readValue(text, User.class);
+        System.out.println(objectUser);
+    }
+
+}
