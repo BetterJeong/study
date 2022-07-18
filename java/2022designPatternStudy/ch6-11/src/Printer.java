e9624aad64751835c75c491b4051a7f7cc87d1b0
Better Jeong <eunjeong42na@gmail.com>
2022-07-18 19:56:17 +0900

    정적 클래스 예제 코드

---
+++ java/2022designPatternStudy/ch6-11/src/Printer.java
@@ -0,0 +1,8 @@
+public class Printer {
+    private static int counter = 0;
+
+    public synchronized static void print(String str) {
+        counter++;
+        System.out.println(str + counter);
+    }
+}
---
+++ java/2022designPatternStudy/ch6-11/src/UserThread.java
@@ -0,0 +1,9 @@
+public class UserThread extends Thread{
+    public UserThread(String name) {
+        super(name);
+    }
+
+    public void run() {
+        Printer.print(Thread.currentThread().getName()+" print using .");
+    }
+}
