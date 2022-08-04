7a3ff95f4212b0d01e69f2d24ee52c9e48cae31b
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-08-04 15:59:22 +0900

    Update HelloWorldControllerTest.java

--- java/SpringBoot/FastCampusJavaSpring/bookmanager/src/test/java/com/example/bookmanager/controller/HelloWorldControllerTest.java
+++ java/SpringBoot/FastCampusJavaSpring/bookmanager/src/test/java/com/example/bookmanager/controller/HelloWorldControllerTest.java
@@ -1,18 +1,18 @@
 package com.example.bookmanager.controller;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
+@AutoConfigureWebMvc
 class HelloWorldControllerTest {
 
     @Autowired
