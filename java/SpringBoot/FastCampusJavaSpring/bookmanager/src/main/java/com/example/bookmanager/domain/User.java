37ff4ec3bae138044c9e1e84c30ee71239768aa0
Better Jeong <eunjeong42na@gmail.com>
2022-08-07 00:06:51 +0900

    jpa repository 실습

--- java/SpringBoot/FastCampusJavaSpring/bookmanager/src/main/java/com/example/bookmanager/domain/User.java
+++ java/SpringBoot/FastCampusJavaSpring/bookmanager/src/main/java/com/example/bookmanager/domain/User.java
@@ -1,16 +1,25 @@
 package com.example.bookmanager.domain;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
     private String name;
+
+    @NonNull
     private String email;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
---
+++ java/SpringBoot/FastCampusJavaSpring/bookmanager/src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `create_at`, `update_at`) values (1, '난정', 'hi@hi.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `create_at`, `update_at`) values (2, '난정2', 'hi2@hi.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `create_at`, `update_at`) values (3, '난정3', 'hi3@hi.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `create_at`, `update_at`) values (4, '난정4', 'hi4@hi.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into user (`id`, `name`, `create_at`, `update_at`) values (5, '난정5', 'hi5@hi.com', now(), now());
