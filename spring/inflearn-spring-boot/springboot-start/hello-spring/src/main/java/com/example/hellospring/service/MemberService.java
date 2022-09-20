16600bd3a146755181b785a9ba7952afa2f3669a
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-09-20 13:43:04 +0900

    스프링 입문

---
+++ spring/inflearn-spring-boot/springboot-start/hello-spring/src/main/java/com/example/hellospring/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.example.hellospring.service;
+
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemberRepository;
+import com.example.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member) {
+        // 중복 검사
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void val
+
+}
