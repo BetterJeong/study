6da6cdad84d8eecb4941c534f4ebe36cae453fa1
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-11-10 13:08:44 +0900

    플레이어 이동

---
+++ unity/inflearn-unity-go/2DGameBasic01/Assets/Movement2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    private float moveSpeed = 5.0f; // 이동 속도
+    private Vector3 moveDirection = Vector3.zero;   // 이동 방향
+
+    private void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(x, y, 0);
+
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+}
