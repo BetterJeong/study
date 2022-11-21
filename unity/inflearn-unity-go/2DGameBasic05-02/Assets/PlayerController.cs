da542c34dcbb5a1918bd016177d7116f747745be
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-11-21 17:24:57 +0900

    애니메이션

---
+++ unity/inflearn-unity-go/2DGameBasic05-02/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 트리거 체크(활성화)
+            animator.SetTrigger("isDie");
+        }
+    }
+
+    public void OnDieEvent()
+    {
+        Debug.Log("End of Die Animation");
+    }
+}
