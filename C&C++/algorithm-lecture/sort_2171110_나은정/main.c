837ddcd461ea0d47e04db850b1459e4ed825bba5
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2022-11-29 20:59:27 +0900

    진행중

--- C&C++/algorithm-lecture/sort_2171110_나은정/main.c
+++ C&C++/algorithm-lecture/sort_2171110_나은정/main.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 50
+#define SWAP(x, y, t) ( (t) = (x), (x) = (y), (y)=(t))
 
-void selection_sort_ascending(int data[]);
-void selection_sort_descending(int data[]);
-void insertion_sort_ascending(int data[]);
-void insertion_sort_descending(int data[]);
-void bubble_sort_ascending(int data[]);
-void bubble_sort_descending(int data[]);
-void merge_sort_ascending(int data[]);
-void merge_sort_descending(int data[]);
-void heap_sort_ascending(int data[]);
-void heap_sort_descending(int data[]);
-void quick_sort_ascending(int data[]);
-void quick_sort_descending(int data[]);
-void radix_sort_ascending(int data[]);
-void radix_sort_descending(int data[]);
-void shell_sort_ascending(int data[]);
-void shell_sort_descending(int data[]);
+void selection_sort_ascending(int data[], int n);
+void selection_sort_descending(int data[], int n);
+void insertion_sort_ascending(int data[], int n);
+void insertion_sort_descending(int data[], int n);
+void bubble_sort_ascending(int data[], int n);
+void bubble_sort_descending(int data[], int n);
+void merge_sort_ascending(int data[], int l, int r, int n);
+void merge_sort_descending(int data[], int l, int r, int n);
+void heap_sort_ascending(int data[], int n);
+void heap_sort_descending(int data[], int n);
+void quick_sort_ascending(int data[], int left, int right);
+void quick_sort_descending(int data[], int left, int right);
+void radix_sort_ascending(int data[], int n);
+void radix_sort_descending(int data[], int n);
+void shell_sort_ascending(int data[], int n);
+void shell_sort_descending(int data[], int n);
 
 int main() {
 	printf("2171110 ³ªÀºÁ¤\n");
@@ -82,158 +83,352 @@ int main() {
 			break;
 		}
 
-		for (int i = 0; i < index; i++) {
-			printf("%d ", data[i]);
-		}
-		printf("\n");
+		int n = index;
 
 		switch (sorting_method) {
 		case 's':
 			switch (sequence) {
 			case 'i':
-				selection_sort_ascending(data);
+				selection_sort_ascending(data, n);
 				break;
 			case 'd':
-				selection_sort_descending(data);
+				selection_sort_descending(data, n);
 				break;
 			}
 			break;
 		case 'i':
 			switch (sequence) {
 			case 'i':
-				insertion_sort_ascending(data);
+				insertion_sort_ascending(data, n);
 				break;
 			case 'd':
-				insertion_sort_ascending(data);
+				insertion_sort_descending(data, n);
 				break;
 			}
 			break;
 		case 'b':
 			switch (sequence) {
 			case 'i':
-				bubble_sort_ascending(data);
+				bubble_sort_ascending(data, n);
 				break;
 			case 'd':
-				bubble_sort_descending(data);
+				bubble_sort_descending(data, n);
 				break;
 			}
 			break;
 		case 'm':
 			switch (sequence) {
 			case 'i':
-				merge_sort_ascending(data);
+				merge_sort_ascending(data, 0, n, n-1);
 				break;
 			case 'd':
-				merge_sort_descending(data);
+				merge_sort_descending(data, 0, n, n-1);
 				break;
 			}
 			break;
 		case 'h':
 			switch (sequence) {
 			case 'i':
-				heap_sort_ascending(data);
+				heap_sort_ascending(data, n);
 				break;
 			case 'd':
-				heap_sort_descending(data);
+				heap_sort_descending(data, n);
 				break;
 			}
 			break;
 		case 'q':
 			switch (sequence) {
 			case 'i':
-				quick_sort_ascending(data);
+				quick_sort_ascending(data, 0, n-1);
 				break;
 			case 'd':
-				quick_sort_descending(data);
+				quick_sort_descending(data, 0, n-1);
 				break;
 			}
 			break;
 		case 'r':
 			switch (sequence) {
 			case 'i':
-				radix_sort_ascending(data);
+				radix_sort_ascending(data, n);
 				break;
 			case 'd':
-				radix_sort_ascending(data);
+				radix_sort_ascending(data, n);
 				break;
 			}
 			break;
 		case 'l':
 			switch (sequence) {
 			case 'i':
-				shell_sort_ascending(data);
+				shell_sort_ascending(data, n);
 				break;
 			case 'd':
-				shell_sort_descending(data);
+				shell_sort_descending(data, n);
 				break;
 			}
 			break;
 		}
+
+		for (int i = 0; i < index; i++) {
+			printf("%d ", data[i]);
+		}
+		printf("\n");
 	}
 
 	return 0;
 }
 
-void selection_sort_ascending(int data[]) {
+void selection_sort_ascending(int data[], int n) {
+	int i, j, min, temp;
+	for (i = 0; i < n - 1; i++) {
+		min = i;
+		for (j = i + 1; j < n; j++) {
+			if (data[j] < data[min]) min = j;
+		}
+		SWAP(data[i], data[min], temp);
+	}
+}
+
+void selection_sort_descending(int data[], int n) {
+	int i, j, max, temp;
+	for (i = 0; i < n - 1; i++) {
+		max = i;
+		for (j = i + 1; j < n; j++) {
+			if (data[j] > data[max]) max = j;
+		}
+		SWAP(data[i], data[max], temp);
+	}
+}
+
+void insertion_sort_ascending(int data[], int n) {
+	int i, j, key;
+	for (i = 1; i < n; i++) {
+		key = data[i];
+		for (j = i - 1; j >= 0 && data[j] > key; j--) {
+			data[j + 1] = data[j];
+		}
+		data[j + 1] = key;
+	}
+}
+
+void insertion_sort_descending(int data[], int n) {
+	int i, j, key;
+	for (i = 1; i < n; i++) {
+		key = data[i];
+		for (j = i - 1; j >= 0 && data[j] < key; j--) {
+			data[j + 1] = data[j];
+		}
+		data[j + 1] = key;
+	}
+}
+
+void bubble_sort_ascending(int data[], int n) {
+	int i, j, temp;
+	for (i = n - 1; i > 0; i--) {
+		for (j = 0; j < i; j++) {
+			if (data[j] > data[j + 1]) {
+				SWAP(data[j], data[j + 1], temp);
+			}
+		}
+	}
+}
+
+void bubble_sort_descending(int data[], int n) {
+	int i, j, temp;
+	for (i = n - 1; i > 0; i--) {
+		for (j = 0; j < i; j++) {
+			if (data[j] < data[j + 1]) {
+				SWAP(data[j], data[j + 1], temp);
+			}
+		}
+	}
+}
+
+void merge_a(int data[], int left, int mid, int right, int n) {
+	int i, j, k, l;
+	i = left; j = mid + 1; k = left;
+	int* sorted = (int*)malloc(sizeof(int) * (n+1));
+
+	while (i <= mid && j <= right) {
+		if (data[i] <= data[j]) {
+			sorted[k++] = data[i++];
+		}
+		else {
+			sorted[k++] = data[j++];
+		}
+	}
+
+	if (i > mid) {
+		for (l = j; l <= right; l++) {
+			sorted[k++] = data[l];
+		}
+	}
+	else {
+		for (l = i; l <= mid; l++) {
+			sorted[k++] = data[l];
+		}
+	}
+	
+	for (l = left; l <= right; l++) {
+		data[l] = sorted[l];
+	}
+}
+
+void merge_d(int data[], int left, int mid, int right, int n) {
+	int i, j, k, l;
+	i = left; j = mid + 1; k = left;
+	int* sorted = (int*)malloc(sizeof(int) * (n + 1));
+
+	while (i <= mid && j <= right) {
+		if (data[i] >= data[j]) {
+			sorted[k++] = data[i++];
+		}
+		else {
+			sorted[k++] = data[j++];
+		}
+	}
+
+	if (i > mid) {
+		for (l = j; l <= right; l++) {
+			sorted[k++] = data[l];
+		}
+	}
+	else {
+		for (l = i; l <= mid; l++) {
+			sorted[k++] = data[l];
+		}
+	}
+
+	for (l = left; l <= right; l++) {
+		data[l] = sorted[l];
+	}
+}
+
+void merge_sort_ascending(int data[], int left, int right, int n) {
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		merge_sort_ascending(data, left, mid, n);
+		merge_sort_ascending(data, mid + 1, right, n);
+		merge_a(data, left, mid, right, n);
+	}
+}
+
+void merge_sort_descending(int data[], int left, int right, int n) {
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		merge_sort_ascending(data, left, mid, n);
+		merge_sort_ascending(data, mid + 1, right, n);
+		merge_d(data, left, mid, right, n);
+	}
+}
+
+void heapify(int* data, int size) {
+	for (int i = 1; i < size; ++i) {
+		int child = i;
+		do {
+			int root = (child - 1) / 2;
+			if (data[root] < data[child]) {
+				int temp = data[root];
+				data[root] = data[child];
+				data[child] = temp;
+			}
+			child = root;
+		} while (child != 0);
+	}
+}
+
+void heap(int* data, int* n) {
+	int temp = data[0];
+	data[0] = data[*n - 1];
+	data[*n - 1] = temp;
+	--(*n);
+}
+
+void heap_sort_ascending(int data[], int n) {
+	for (int i = 0; i < n; i++) {
+		heapify(data, n);
+		heap(data, &n);
+	}
+}
+
+void heap_sort_descending(int data[], int n) {
+	for (int i = 0; i < n; i++) {
+		heapify(data, n);
+		heap(data, &n);
+	}
+}
+
+int partition_a(int data[], int left, int right) {
+	int pivot, temp;
+	int low, high;
+
+	low = left;
+	high = right + 1;
+	pivot = data[left];
+	do {
+		do {
+			low++;
+		} while (data[low] < pivot);
+		do {
+			high--;
+		} while (data[high] > pivot);
+		if (low < high) SWAP(data[low], data[high], temp);
+	} while (low < high);
+
+	SWAP(data[left], data[high], temp);
+	return high;
+}
+
+int partition_d(int data[], int left, int right) {
+	int pivot, temp;
+	int low, high;
+
+	low = left;
+	high = right + 1;
+	pivot = data[left];
+	do {
+		do {
+			low++;
+		} while (data[low] > pivot);
+		do {
+			high--;
+		} while (data[high] < pivot);
+		if (low < high) SWAP(data[high], data[low], temp);
+	} while (low < high);
+
+	SWAP(data[high], data[left], temp);
+	return high;
+}
+
+void quick_sort_ascending(int data[], int left, int right) {
+	if (left < right) {
+		int q = partition_a(data, left, right);
+		quick_sort_ascending(data, left, q - 1);
+		quick_sort_ascending(data, q + 1, right);
+	}
+}
+
+void quick_sort_descending(int data[], int left, int right) {
+	if (left < right) {
+		int q = partition_d(data, left, right);
+		quick_sort_ascending(data, left, q - 1);
+		quick_sort_ascending(data, q + 1, right);
+	}
+}
+
+void radix_sort_ascending(int data[], int n) {
 
 }
 
-void selection_sort_descending(int data[]) {
+void radix_sort_descending(int data[], int n) {
 
 }
 
-void insertion_sort_ascending(int data[]) {
+void shell_sort_ascending(int data[], int n) {
 
 }
 
-void insertion_sort_descending(int data[]) {
-
-}
-
-void bubble_sort_ascending(int data[]) {
-
-}
-
-void bubble_sort_descending(int data[]) {
-
-}
-
-void merge_sort_ascending(int data[]) {
-
-}
-
-void merge_sort_descending(int data[]) {
-
-}
-
-void heap_sort_ascending(int data[]) {
-
-}
-
-void heap_sort_descending(int data[]) {
-
-}
-
-void quick_sort_ascending(int data[]) {
-
-}
-
-void quick_sort_descending(int data[]) {
-
-}
-
-void radix_sort_ascending(int data[]) {
-
-}
-
-void radix_sort_descending(int data[]) {
-
-}
-
-void shell_sort_ascending(int data[]) {
-
-}
-
-void shell_sort_descending(int data[]) {
+void shell_sort_descending(int data[], int n) {
 
 }
