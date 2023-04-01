b54cbeb24b7358548ae46c08d6ee6acf4ef751da
Better Jeong <eunjeong42na@gmail.com>
2023-04-01 23:23:49 +0900

    안드

---
+++ android/do-it-android/ch06/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val name = TextView(this).apply {
+            typeface = Typeface.DEFAULT_BOLD
+            text = "Lake Louise"
+        }
+    }
+}
