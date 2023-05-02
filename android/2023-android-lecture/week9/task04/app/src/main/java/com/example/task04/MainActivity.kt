89f66a19d2672cc150902da911e6b76e4a438d9d
Better Jeong <eunjeong42na@gmail.com>
2023-05-02 22:44:38 +0900

    Fragment, navigation

--- android/2023-android-lecture/week9/task04/app/src/main/java/com/example/task04/MainActivity.kt
+++ android/2023-android-lecture/week9/task04/app/src/main/java/com/example/task04/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.task04
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,16 +22,30 @@ class MainActivity : AppCompatActivity() {
 class HomeFragment : Fragment(R.layout.home_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModel : MyViewModel by activityViewModels()
+        view.findViewById<TextView>(R.id.textView).text = viewModel.myValue.value
+
         view.findViewById<Button>(R.id.button).setOnClickListener {
+            viewModel.myValue.value = (Integer.parseInt(viewModel.myValue.value.toString()) + 1).toString()
             findNavController().navigate(R.id.action_homeFragment_to_nav1Fragment)
         }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
     }
 }
 
 class Nav1Fragment : Fragment(R.layout.nav1_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModel : MyViewModel by activityViewModels()
+        view.findViewById<TextView>(R.id.textView).text = viewModel.myValue.value
+
         view.findViewById<Button>(R.id.button).setOnClickListener {
+            viewModel.myValue.value = (Integer.parseInt(viewModel.myValue.value.toString()) + 1).toString()
             findNavController().navigate(R.id.action_nav1Fragment_to_nav2Fragment)
         }
     }
@@ -35,7 +54,11 @@ class Nav1Fragment : Fragment(R.layout.nav1_fragment) {
 class Nav2Fragment : Fragment(R.layout.nav2_fragment) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val viewModel : MyViewModel by activityViewModels()
+        view.findViewById<TextView>(R.id.textView).text = viewModel.myValue.value
+
         view.findViewById<Button>(R.id.button).setOnClickListener {
+            viewModel.myValue.value = (Integer.parseInt(viewModel.myValue.value.toString()) + 1).toString()
             findNavController().navigate(R.id.action_nav2Fragment_to_homeFragment)
         }
     }
--- android/2023-android-lecture/week9/task04/app/src/main/java/com/example/task04/MyViewModel.kt
+++ android/2023-android-lecture/week9/task04/app/src/main/java/com/example/task04/MyViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.task04
 
-class MyViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel : ViewModel() {
+    val myValue = MutableLiveData<String>()
+
+    init {
+        myValue.value = "0"
+    }
 }
